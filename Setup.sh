09a50ce5370413a364250ad6131f9842ba0e0148
Miguel angel Florido cifuentes <mflorido@c2r11s4.42madrid.com>
2019-11-18 11:13:22 +0100

    Setup

--- Setup.sh
+++ Setup.sh
@@ -1,4 +1,13 @@
-cp -f .zshenv $HOME/
+#cp -f .zshenv $HOME/
+unset ZDOTDIR
+unset VIMINIT
+touch $HOME/.zshenv
+if ! cat $HOME/.zshenv | grep -q "export ZDOTDIR='$HOME/.config/zsh'"; then
+	echo "export ZDOTDIR='$HOME/.config/zsh'">> "$HOME/.zshenv";
+fi
+if ! cat $HOME/.zshenv | grep -q "export VIMINIT='source \$HOME/.config/vim/.vimrc'"; then
+	echo "export VIMINIT='source \$HOME/.config/vim/.vimrc'" >> "$HOME/.zshenv"
+fi
 cp -rf .config $HOME/
 cp -rf .config/zsh/.zshrc $HOME/
 cp -rf .config/vim/.vimrc $HOME/
@@ -6,6 +15,7 @@ if [ -d "$HOME/.config/zsh/zsh-syntax-highlighting" ]; then
 	rm -rf $HOME/.config/zsh/zsh-syntax-highlighting
 fi
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.config/zsh/zsh-syntax-highlighting
+cd $HOME
 clear;
 echo "MIGUELez11's Configuration has been loaded successfully."
 echo ""
